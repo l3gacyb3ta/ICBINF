729e9639942a63965c1b185a72e833f03870f6be
Arcade Wise <l3gacy.b3ta@gmail.com>
2026-03-19 19:36:17 -0500

    bad news, base urls

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-const BASE_URL = process.env.BASE_URL || '';
+const BASE_URL = process.env.BASE_URL || 'https://arcades.agency/ICBINF';
 
 export default {
-    "base": `/${BASE_URL}`,
+    "base": `${BASE_URL}`,
     "root": "src",
     "build": {
         outDir: "../dist"
